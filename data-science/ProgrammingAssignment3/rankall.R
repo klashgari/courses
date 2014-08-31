d5af69785723e3b332f29951b1e6b90484425723
klashgari <kourosh.dev@gmail.com>
2014-08-31 00:30:07 -0700

    HW3

---
+++ data-science/ProgrammingAssignment3/rankall.R
@@ -0,0 +1,63 @@
+#  rankhospital takes three arguments: the 2-character abbreviated name of a
+# state (state), an outcome (outcome), and the ranking of a hospital in that state for that outcome (num).
+# The function reads the outcome-of-care-measures.csv file and returns a character vector with the name
+# of the hospital that has the ranking specied by the num argument. 
+# example: 
+#     rankhospital("MD", "heart failure", 5)
+#     tail(rankall("pneumonia", "worst"), 3)
+#
+rankall <- function(outcome , num = "best"){
+
+  rank <- function(d, colName) {
+    # sort
+    sortedData <- d[order(d[colName], d["Hospital.Name"] ),]
+    
+    rowCount <- nrow(sortedData)    
+    if( num == "best"){
+      index <- 1
+    }
+    else if( num == "worst"){
+      index <- rowCount 
+    }
+    else if( is.numeric(x=num) && ( num  < rowCount || num >  0) ){
+      index <- as.numeric(x=num) 
+    }
+    else{
+      stop("invalid num")
+      return
+    }
+    
+    ## Return hospital name in  state
+    result <- sortedData[ index , ]$Hospital.Name
+    result 
+  }
+  
+  
+  ## Read outcome data
+  f <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+  data <- data.frame(f, ignore.case = TRUE)
+  
+  # Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack
+  pattern <- sprintf("^Hospital.30.Day.Death..Mortality..Rates.from.%s$" , gsub("\\s", ".", outcome ))  
+  outcomeColIndex <- grep(pattern, names(data), ignore.case=TRUE)
+  
+  
+  ## Check that outcome is valid
+  if( !any(outcomeColIndex) ){
+    stop("invalid outcome")
+    return
+  }
+  
+  data[, outcomeColIndex] <- suppressWarnings( as.numeric(x= data[, outcomeColIndex] ) )
+  data <- data[complete.cases(data), ] # remove NA
+
+ 
+  outcomeColName <- colnames(data)[outcomeColIndex]
+  partitionedData<- split( data[, c( "Hospital.Name", "State", outcomeColName )], data$State)
+  result <- lapply(partitionedData, rank, outcomeColName)
+  
+  
+  data.frame(hospital = unlist(result), 
+             state = names(result), 
+             row.names = names(result))
+}
